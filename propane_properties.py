150bd5d602ee2aa90321869cc4a676f459baadb2
Markus Hays Nielsen <markushays@whitson.com>
2020-12-10 15:29:10 +0100

    dens_calc: Define density calculation

---
+++ propane_properties.py
@@ -0,0 +1,14 @@
+
+def calculate_density(p: float, T: float, dens_type: DensType):
+    """
+    Some text
+
+    Some more text
+
+    Final part of text
+    """
+    
+    dens_type = dens_type.value
+    
+    density = 1
+    return density
