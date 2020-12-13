25623f4d141d058ca93b2fa4d320efceab497ded
Markus Hays Nielsen <markushays@whitson.com>
2020-12-13 17:33:17 +0100

    Small update to example

--- example_calculation.py
+++ example_calculation.py
@@ -18,7 +18,7 @@ minimum_temperature = 259.67  # Units [R]
 maximum_temperature = 859.67  # Units [R]
 minimum_pressure = 0.0234  # Units [psia]
 maximum_pressure = 1724.0  # Units [psia]
-N = 500  # (Optional) Number of grids in plot. Default is 50 in none is given
+N = 100  # (Optional) Number of grids in plot. Default is 50 in none is given
 calc_density_heatmap(
     minimum_pressure, maximum_pressure, minimum_temperature, maximum_temperature, N
 )
